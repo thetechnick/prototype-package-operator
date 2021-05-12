bdc186be854ead74435f4b8cdea7e9a7b70fdd8e
Nico Schieder <code@nico-schieder.de>
2021-05-12 16:50:16 +0200

    INIT!
    Signed-off-by: Nico Schieder <code@nico-schieder.de>

---
+++ apis/apis.go
@@ -0,0 +1,8 @@
+package apis
+
+import (
+	"github.com/thetechnick/package-operator/apis/packages"
+)
+
+// AddToScheme adds all api Resources to the Scheme
+var AddToScheme = packages.AddToScheme
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/thetechnick/package-operator
+
+go 1.16
+
+require (
+	k8s.io/apimachinery v0.21.0
+	sigs.k8s.io/controller-runtime v0.8.3
+)
